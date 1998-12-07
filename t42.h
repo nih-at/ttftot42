b7b27d43ca7c05e3629bc4b1012ad84c0561d795
Dieter Baron <dillo@nih.at>
1998-12-07 01:58:03 +0000

    added copyright notice

--- t42.h
+++ t42.h
@@ -1,3 +1,30 @@
+#ifndef _HAD_T42_H
+#define _HAD_T42_H
+
+/*
+  t42.h -- general header file
+  Copyright (C) 1998 Dieter Baron
+
+  This file is part of otftot42, to use TrueType fonts in PostScript.
+  The author can be contacted at <dillo@giga.or.at>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+
 #include <stdio.h>
 #include <freetype.h>
 
@@ -62,3 +89,5 @@ void close_font(font *f);
 char *get_name(TT_Face f, int nnames, int name);
 int write_t42(font *f, FILE *fout);
 int write_afm(font *f, FILE *fout);
+
+#endif /* t42.h */
