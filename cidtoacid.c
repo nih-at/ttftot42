7467cd21efeaaa537bb1124c7658c17dd127a8ef
Dieter Baron <dillo@nih.at>
1999-05-05 12:55:44 +0000

    fixed alpha bug (8 byte longs)

--- cidtoacid.c
+++ cidtoacid.c
@@ -24,6 +24,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <getopt.h>
 #include <errno.h>
 
--- util.c
+++ util.c
@@ -137,6 +137,7 @@ write_sfnts(font *f, FILE *fout)
     }
     checksum &= 0xffffffff;
     checksum = 0xb1b0afba - checksum;
+    checksum &= 0xffffffff;
 
     soff = 0;
     slen = HEADER_LEN;
@@ -179,7 +180,9 @@ write_tabledir_entry(FILE *fout, struct table *t)
 {
     fprintf(fout, "%02.2X%02.2X%02.2X%02.2X%08.8lX%08.8lX%08.8lX\n",
 	    t->tag[0], t->tag[1], t->tag[2], t->tag[3],
-	    t->checksum, t->offset, t->length);
+	    t->checksum & 0xffffffff,
+	    t->offset & 0xffffffff,
+	    t->length & 0xffffffff);
 
     return 0;
 }
