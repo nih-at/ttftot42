6d8b751d57bb5467172280483fc63937ae6a909e
Dieter Baron <dillo@nih.at>
2001-11-18 15:53:06 +0000

    add copyright notice

--- cid.h
+++ cid.h
@@ -1,6 +1,30 @@
 #ifndef _HAD_CID_H
 #define _HAD_CID_H
 
+/*
+  cid.h -- header file for CID font handling
+  Copyright (C) 1998, 1999 Dieter Baron
+
+  This file is part of ttftot42, to use TrueType fonts in PostScript.
+  The author can be contacted at <dillo@giga.or.at>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+
 /* command codes */
 
 #define C_none	1      /* no active code */
